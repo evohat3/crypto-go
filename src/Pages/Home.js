3b9fa012140938d0030efae1cb8d8d853424e9e2
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-12-13 23:28:16 -0600

    added more hover styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import "../App.css"
-
+import React, { useState, useEffect } from "react";
+import "../App.css";
 
 function Home() {
   const [coins, setCoins] = useState([]);
@@ -10,25 +9,25 @@ function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const apiKey = '8ae759da67msh17660d17a33b0aep134bc6jsn4d902f5fbb19'; // Replace with your actual API key
-        const url = 'https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0';
-        
+        const apiKey = "8ae759da67msh17660d17a33b0aep134bc6jsn4d902f5fbb19"; // Replace with your actual API key
+        const url =
+          "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0";
+
         const response = await fetch(url, {
-          method: 'GET',
+          method: "GET",
           headers: {
-            'X-RapidAPI-Key': apiKey, // Add your API key here
-            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+            "X-RapidAPI-Key": apiKey, // Add your API key here
+            "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
           },
         });
 
         if (!response.ok) {
-          throw new Error('Failed to fetch data');
+          throw new Error("Failed to fetch data");
         }
 
         const result = await response.json();
-        console.log(result)
+        console.log(result);
         const coinData = result.data.coins || [];
-        
 
         setCoins(coinData);
         setLoading(false);
@@ -51,12 +50,28 @@ function Home() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 m-4 p-4 pill">
       {coins.map((coin) => (
-        <div key={coin.uuid} className="border-4 bg-black p-2 sm:p-1 md:p-2 rounded-full ">
-          <div className={`truncate text-base ${coin.change < 0 ? 'bg-red-500' : 'bg-green-500'} border-4 border-white rounded-full`}>
-          <span className={`block p-4 bg-black text-center ${coin.change < 0 ? 'text-red-500 hover:bg-red-500 hover:text-black' : 'text-green-500 hover:bg-green-500 hover:text-black'} text-lg`}>
-  {coin.symbol} <span className="float-right">Change {coin.change}%</span>
-</span>
-            <span className="block p-4 text-center">Price: ${(Number(coin.price).toLocaleString())}</span>
+        <div
+          key={coin.uuid}
+          className={`border-4 p-2 sm:p-1 md:p-2 rounded-full hover:bg-slate-400`}
+        >
+          <div
+            className={`truncate text-base ${
+              coin.change < 0 ? "bg-red-500" : "bg-green-500"
+            } border-4 border-white rounded-full`}
+          >
+            <span
+              className={`block p-4 bg-black text-center ${
+                coin.change < 0
+                  ? "text-red-500 hover:bg-red-500 hover:text-black hover:border-b-4 border-black"
+                  : "text-green-500 hover:bg-green-500 hover:text-black hover:border-b-4 border-black"
+              } text-lg`}
+            >
+              {coin.symbol}{" "}
+              <span className="float-right">Change {coin.change}%</span>
+            </span>
+            <span className="block p-4 text-center ">
+              Price: ${Number(coin.price).toLocaleString()}
+            </span>
           </div>
         </div>
       ))}
