add2454a1e2c07b8374de537e51a2f3ba0ea7066
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-12-16 02:38:49 -0600

    create event handler to update url params when clicked

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Home() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const apiKey = "8ae759da67msh17660d17a33b0aep134bc6jsn4d902f5fbb19"; // Replace with your actual API key
+        const apiKey = process.env.API_KEY;
         const url =
           "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0";
 
         const response = await fetch(url, {
           method: "GET",
           headers: {
-            "X-RapidAPI-Key": apiKey, // Add your API key here
+            "X-RapidAPI-Key": apiKey,
             "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
           },
         });
@@ -24,7 +27,6 @@ function Home() {
         }
 
         const result = await response.json();
-        console.log(result);
         const coinData = result.data.coins || [];
 
         setCoins(coinData);
@@ -38,6 +40,11 @@ function Home() {
     fetchData();
   }, []);
 
+  const handleCoinClick = (coinSymbol) => {
+    // Navigate to the /:id route with the symbol as the parameter
+    navigate(`/${coinSymbol}`);
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -45,38 +52,38 @@ function Home() {
   if (error) {
     return <div>Error: {error.message}</div>;
   }
+
   return (
     <div className="p-8 bg-black">
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 m-4 p-4">
-      {coins.map((coin) => (
-        
-        <div
-          key={coin.uuid}
-          className={`border-4 p-2 sm:p-1 md:p-2 rounded-full hover:bg-slate-400`}
-        >
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 m-4 p-4">
+        {coins.map((coin) => (
           <div
-            className={`truncate text-base ${
-              coin.change < 0 ? "bg-red-500" : "bg-green-500"
-            } border-4 border-white rounded-full`}
+            key={coin.uuid}
+            className={`border-4 p-2 sm:p-1 md:p-2 rounded-full hover:bg-slate-400`}
+            onClick={() => handleCoinClick(coin.symbol)}
           >
-            <span
-              className={`block p-4 bg-black text-center ${
-                coin.change < 0
-                  ? "text-red-500 hover:bg-red-500 hover:text-black hover:border-b-4 border-black"
-                  : "text-green-500 hover:bg-green-500 hover:text-black hover:border-b-4 border-black"
-              } text-lg`}
+            <div
+              className={`truncate text-base ${
+                coin.change < 0 ? "bg-red-500" : "bg-green-500"
+              } border-4 border-white rounded-full`}
             >
-              {coin.symbol}{}
-              <span className="float-right">Change {coin.change}%</span>
-            </span>
-            <span className="block p-4 text-center ">
-              Price: ${Number(coin.price).toLocaleString()}
-
-            </span>
+              <span
+                className={`block p-4 bg-black text-center ${
+                  coin.change < 0
+                    ? "text-red-500 hover:bg-red-500 hover:text-black hover:border-b-4 border-black"
+                    : "text-green-500 hover:bg-green-500 hover:text-black hover:border-b-4 border-black"
+                } text-lg`}
+              >
+                {coin.symbol}
+                <span className="float-right">Change {coin.change}%</span>
+              </span>
+              <span className="block p-4 text-center ">
+                Price: ${Number(coin.price).toLocaleString()}
+              </span>
+            </div>
           </div>
-        </div>
-      )) }, {console.log('coins', coins)}
-    </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Search() {
   return (
-    <div>Search</div>
+    <div className='bg-black'>
+        <div className='background-blue-400 m-4 p-4 text-white text-center'>Search</div>
+        <div className=''></div>
+
+    </div>
+    
   )
 }
